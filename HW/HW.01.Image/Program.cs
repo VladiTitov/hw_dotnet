4d07063b70c4f117d888ed01f524e43f3e3db959
Vladislav Titov <VladiTitov@users.noreply.github.com>
2021-06-17 17:35:21 +0300

    First task complete

---
+++ HW/HW.01.Image/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace HW._01.Image
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader textReader = new StreamReader("./Data/image.txt",true);
+
+            string textReaderResult = textReader.ReadToEnd();
+            string[] arrayOfTextResult = textReaderResult.Split(' ');
+            byte[] imageBytes = new byte[arrayOfTextResult.Length - 1];
+            for (int i = 0; i < arrayOfTextResult.Length - 1; i++)
+            {
+                byte binary = Convert.ToByte(arrayOfTextResult[i], 2);
+                imageBytes[i] = binary;
+            }
+
+            File.WriteAllBytes(@"./Data/result.png", imageBytes);
+            textReader.Dispose();
+        }
+    }
+}
