d010b4128dd015bb7d8bc0a039719f70a6168dac
Vladislav Titov <VladiTitov@users.noreply.github.com>
2021-06-21 09:25:00 +0300

    first task complete

---
+++ HomeWorks/HW03.Operators/Task_01.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HW03.Operators
+{
+    class Task_01
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число: a=");
+            int a = InputOutput();
+            Console.WriteLine("Введите первое число: b=");
+            int b = InputOutput();
+
+            Console.WriteLine($"Результат сложения a + b = {GetSum(a, b)}");
+            Console.ReadLine();
+        }
+
+        private static int InputOutput()
+        {
+            int value;
+            while (!int.TryParse(Console.ReadLine(), out value))
+            {
+                Console.WriteLine("Ошибка! Введите корректное число!");
+            }
+            return value;
+        }
+
+        private static int GetSum(int a, int b) => a + b;
+
+    }
+}
