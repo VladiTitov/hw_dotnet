23db3767e24d99f398140bfa9887937642e91dba
Vladislav Titov <VladiTitov@users.noreply.github.com>
2021-06-25 17:10:21 +0300

    third task complete

---
+++ HomeWorks/HW04.Task03/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace HW04.Task03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Task(100).Start();
+            Console.ReadLine();
+        }
+    }
+}
