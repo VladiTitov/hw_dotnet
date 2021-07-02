8d98639555c8fa8a59547ab345d509bea641a26e
Vladislav Titov <VladiTitov@users.noreply.github.com>
2021-07-02 09:24:01 +0300

    first task complete

--- HomeWorks/HW06.AssemblyOne/SportBike.cs
+++ HomeWorks/HW06.AssemblyOne/SportBike.cs
@@ -2,6 +2,12 @@
 {
     public class SportBike : Motorcycle
     {
-        public SportBike(string manufacturer) : base(manufacturer) { }
+        public SportBike(string manufacturer) : base(manufacturer)
+        {
+            BikeModel = "SportBike";
+            //Manufacturer = "";                модификатор private в деле
+            weightPrivateProtected = 150;       //модификатор private установлен совместно с protected, который и срабатывает в данном случае
+            Stop();                             //protected в деле
+        }
     }
 }
