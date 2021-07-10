89e36030bd0ad0b35a052dd244fd3a95f7072a09
Vladislav Titov <capt.Hoffmann@gmail.com>
2021-07-10 22:00:11 +0300

    fourth task complete

---
+++ HomeWorks/HW07.Booking.Com/Models/Room.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HW07.Booking.Com.Models
+{
+    class Room
+    {
+        public int Num { get; }
+        public double Area { get; }
+        public bool IsFree { get; set; }
+        public int BedsCount { get; }
+        public double Cost { get; set; }
+
+        public Room(int num, double area, int bedsCount) => (Num, Area, BedsCount) = (num, area, bedsCount);
+
+        public void ShowRoom() =>
+            Console.WriteLine($"Room num:{Num}, Room area: {Area}, Room cost: {Cost}");
+    }
+}
