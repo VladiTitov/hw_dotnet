4cb6fe5682b9dd43723bc436d31215ece52cc28e
Vladislav Titov <capt.Hoffmann@gmail.com>
2021-07-17 17:03:52 +0300

    first task comlpete

---
+++ HomeWorks/HW08.Task01/Models/TeamOrTeachLeader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace HW08.Task01.Models
+{
+    class TeamOrTeachLeader : Engineer
+    {
+        public override List<string> Responsibilities { get; set; } = new List<string>()
+        {
+            "Ability to combine technically competent solutions with quality development processes"
+        };
+        public override List<string> Technologies { get; set; } = new List<string>()
+        {
+            "Design patterns",
+            "Managment skills",
+            "SOLID",
+        };
+
+        public TeamOrTeachLeader(string firstName, string secondName, int experience, string github) : base(firstName, secondName, experience, github) { }
+    }
+}
