0608cd5d6d834173be8ea427e1fef590fa40c174
Vladislav Titov <capt.Hoffmann@gmail.com>
2021-07-17 17:36:22 +0300

    third task complete

---
+++ HomeWorks/HW08.Task03/Models/SeniorDeveloper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW08.Task03.Models
+{
+    class SeniorDeveloper : Engineer
+    {
+        public override List<string> Responsibilities { get; set; } = new List<string>()
+        {
+            "Possession of practical development experience",
+            "Ability to make the right technological decisions in a project",
+            "Ability to solve slightly more complex problems, to do it faster or better"
+        };
+        public override List<string> Technologies { get; set; } = new List<string>()
+        {
+            "Design patterns",
+            "Managment skills",
+            "SOLID",
+            "HTML,CSS,JS"
+        };
+        public SeniorDeveloper(string firstName, string secondName, int experience, string github) : base(firstName, secondName, experience, github) { }
+    }
+}
